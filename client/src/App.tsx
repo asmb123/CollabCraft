32718435e1087c056d474792ef48453abe4c41a6
asmb123 <asmit.barua.phe23@itbhu.ac.in>
2025-06-15 14:55:04 +0530

    shadcn react client setup

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { Toaster } from "sonner"
+// import { toast } from "sonner"
+// import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <main className="dark w-screen h-screen bg-background flex flex-col justify-center items-center">
+        <Toaster/>
+      </main>
+    </>
+  )
+}
+
+export default App
