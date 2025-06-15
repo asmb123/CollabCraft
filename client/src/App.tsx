a515fb239627d31602fcb35be4bc870f1f1c255a
asmb123 <asmit.barua.phe23@itbhu.ac.in>
2025-06-15 16:03:00 +0530

    form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Toaster } from "sonner"
+import { ProfileForm } from "./components/Form"
 // import { toast } from "sonner"
 // import { Button } from "@/components/ui/button"
 
@@ -6,8 +7,12 @@ function App() {
 
   return (
     <>
-      <main className="dark w-screen h-screen bg-background flex flex-col justify-center items-center">
-        <Toaster/>
+      <main className="w-screen h-screen bg-background flex flex-col justify-center items-center">
+        <Toaster />
+        <div className="w-screen md:w-2/5 h-1/2 md:h-2/3 flex flex-col justify-center items-center border-2 rounded-2xl">
+          <p className="w-full mb-10 text-center text-2xl md:text-3xl font-bold">Welcome to CollabCraft</p>
+          <ProfileForm />
+        </div>
       </main>
     </>
   )
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider } from './components/ThemeProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  </StrictMode >,
 )
